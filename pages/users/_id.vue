a44dfc0876742146f1e6cc7e5a0d6e7581840fec
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-08-31 22:15:38 +0900

    Add user id page

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        <p>
-            /users/_id.vue
-        </p>
-    </div>
+  <div>
+    <p>
+      User ID: {{userId}}
+    </p>
+  </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        userId: this.$route.params.id
+      }
+    }
+  }
+</script>
