f59397230b751afabd046f2bebefadeabcf5d75b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-09-01 00:56:03 +0900

    Add posts page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-          Hello, world
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <h3>Nuxt.jsのタグがつけられた投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}}</span>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">{{item.user.id}}</nuxt-link>
+            </small>
+          </h4>
+
+          <div>{{item.body.slice(0, 130)}}......</div>
+          <p><a :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -28,6 +26,13 @@ import AppLogo from '~/components/AppLogo.vue'
 export default {
   components: {
     AppLogo
+  },
+
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('http://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   }
 }
 </script>
@@ -35,10 +40,7 @@ export default {
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 16px;
 }
 
 .title {
@@ -60,6 +62,20 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
 }
 </style>
 
